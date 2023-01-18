1c7cbe01f08edc6e7288e3ec7f71058c787d8426
Ajaya <ajayakumal21@gmail.com>
2023-01-18 09:14:01 +0545

    pipeline

--- Pipeline.R
+++ Pipeline.R
@@ -1,3 +1,11 @@
-data(flights, packages ="nycflights13")
-print(flights)
-flights %>% filter(month==1,day==6)
+data(flights, package ="nycflights13")
+View(flights)
+a = flights %>% filter(carrier=="AA")
+print(a)
+flights %>% filter(carrier=="AA"|carrier=="UA")%>% ggplot(aes(x=dep_delay))+geom_line(aes(y=dep_time,col="red")) + facet_wrap(~carrier)
+
+data(flights, package="nycflights13")
+select(flights, year, day, month) %>%arrange(year,desc(day),desc(month))
+
+
+filter(flights, month==4, day ==6)
